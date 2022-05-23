83ebdc86a576a7b9df6ff79f00bcdef40bb2d599
User <sean.al.baroudi@gmail.com>
2022-05-23 21:04:03 +0100

    Almost done first section

--- 6a_ReactReduxCourse/reactportfolio/src/App.js
+++ 6a_ReactReduxCourse/reactportfolio/src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 
-class RegularClass {}
-class ComponentClass extends Component {}
-
-const regularClassInstance = new RegularClass();
-const componentClassInstance = new ComponentClass();
-
-console.log(regularClassInstance);
-console.log(componentClassInstance);
-
-
 class App extends Component {
+  constructor() {
+    super(); //you can't extend a class, and not initialize superclass fields.
+    this.state = { displayBio: false }; //toggles bio
+
+    this.toggleDisplayBio = this.toggleDisplayBio.bind(this); //must bind methods to access this.
+  }
+
+
+  toggleDisplayBio() {
+    this.setState({ displayBio: !this.state.displayBio});
+  }
+
   render() { //defines the HTML elements structure of the Component. This is a static method in Component Class.
       return ( <div>
         <h1> Hello! </h1>
         <p> My name is Bill. I'm a Software Engineer. </p>
         <p> I'm looking forward to learning React.js </p>
+        <button onClick={this.toggleDisplayBio}>Show Less</button>
+        { this.state.displayBio? (
+           <div>
+           <p> I live in Colarado. I like cooking, hiking and reading. </p>
+           </div>
+         ): (
+           <div>
+              <button onClick={this.toggleDisplayBio}> Read More </button>
+           </div>
+         )}
+
         </div> ) //Usage of special JSX
   }
 }
--- 6a_ReactReduxCourse/reactportfolio/src/index.js
+++ 6a_ReactReduxCourse/reactportfolio/src/index.js
@@ -8,35 +8,3 @@ import App from './App';
 //We just use JSX tags with the App's name, to render it in the DOM tree.
 ReactDOM.render(<App />, document.getElementById("root"));
 //We use a self-enclosing tag, as our App has no child dependencies.
-
-class Animal {
-  constructor(name,age) {
-    this.name=name;
-    this.age=age;
-  }
-
-  speak() {
-    console.log("I am", this.name, "and I am", this.age, "years old.");
-  }
-}
-
-//This will print out to inspector console.
-const animal1 = new Animal("Simba", 3);
-animal1.speak();
-
-class Lion extends Animal {
-  constructor(name,age,furColor, speed) {
-      super(name, age);
-      this.furColor = furColor;
-      this.speed = speed;
-  }
-  roar() {
-    console.log("Roar! I have " , this.furColor, " colored fur.");
-  }
-}
-
-const lion1 = new Lion("Mufasa", 20, "gold");
-const lion2 = new Lion("Scar", 19, "brown");
-
-lion1.roar();
-lion2.roar();
